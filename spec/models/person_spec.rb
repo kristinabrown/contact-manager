132b3ca244d4e926959d3d6e79517f9fae9b5db9
Kristina Brown <kristina.frey.frey@gmail.com>
2015-04-16 14:29:30 -0600

    implement validations on person

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Person, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is invalid witout a first name" do
+    person = Person.new(first_name: nil, last_name: "smith")
+    
+    expect(person).not_to be_valid
+  end
+  
+  it "is invalid without a last name" do
+    person = Person.new(first_name: "jane", last_name: nil)
+    
+    expect(person).not_to be_valid
+  end
 end
+
