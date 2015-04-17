91ed145ae511d7535c3d85058b6b454018b22d59
Kristina Brown <kristina.frey.frey@gmail.com>
2015-04-17 13:50:43 -0600

    initial email address tests passing

--- spec/models/email_address_spec.rb
+++ spec/models/email_address_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe EmailAddress, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:email_address) { EmailAddress.new(address: "example@example.org") }
+  
+  it 'is a valid email address' do
+    expect(email_address).to be_valid
+  end
+  
+  it 'is valid without an address' do
+    email_address.address = nil
+    
+    expect(email_address).to_not be_valid
+  end
 end
