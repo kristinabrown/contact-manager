404d66c592596fb6d50ad2322e832092ddd278e1
Kristina Brown <kristina.frey.frey@gmail.com>
2015-04-19 14:23:57 -0600

    Contact module added

--- app/controllers/phone_numbers_controller.rb
+++ app/controllers/phone_numbers_controller.rb
@@ -1,17 +1,13 @@
 class PhoneNumbersController < ApplicationController
-  before_action :set_phone_number, only: [:show, :edit, :update, :destroy]
+  before_action :find_resource, only: [:edit, :update, :destroy]
 
-  # GET /phone_numbers/new
   def new
     @phone_number = PhoneNumber.new(contact_id: params[:contact_id], contact_type: params[:contact_type])
   end
 
-  # GET /phone_numbers/1/edit
   def edit
   end
 
-  # POST /phone_numbers
-  # POST /phone_numbers.json
   def create
     @phone_number = PhoneNumber.new(phone_number_params)
 
@@ -40,8 +36,6 @@ class PhoneNumbersController < ApplicationController
     end
   end
 
-  # DELETE /phone_numbers/1
-  # DELETE /phone_numbers/1.json
   def destroy
     @phone_number.destroy
     respond_to do |format|
@@ -51,12 +45,6 @@ class PhoneNumbersController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_phone_number
-      @phone_number = PhoneNumber.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
     def phone_number_params
       params.require(:phone_number).permit(:number, :contact_id, :contact_type)
     end
