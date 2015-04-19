ae4b3460d2092bc2d9bb8f58f5ca31096382675f
Kristina Brown <kristina.frey.frey@gmail.com>
2015-04-19 13:35:09 -0600

    company emails working

--- spec/features/company_view_spec.rb
+++ spec/features/company_view_spec.rb
@@ -62,4 +62,64 @@ let(:company) { Company.create(name: "Apple") }
       expect(page).to_not have_content(phone_number)
     end
   end
+  
+  describe 'email addresses', type: :feature do
+    
+    before(:each) do 
+      company.email_addresses.create(address: "sample@example.org")
+      company.email_addresses.create(address: "test@example.org")
+      
+      visit company_path(company)
+    end
+ 
+  
+  it 'has lis' do
+    company.email_addresses.each do |email|
+      expect(page).to have_selector('li', text: email.address)
+    end
+  end
+  
+  it 'has a link to add more emails' do
+    expect(page).to have_link("Add email address", href: new_email_address_path(contact_id: company.id, contact_type: 'Company'))
+  end
+  
+  it 'has an add email address link' do 
+    page.click_link('Add email address', href: new_email_address_path(contact_id: company.id, contact_type: 'Company'))
+    page.fill_in('Address', with: 'new.example@example.org')
+    page.click_button('Create Email address')
+    
+    expect(current_path).to eq(company_path(company))
+    expect(page).to have_content('new.example@example.org') 
+  end
+  
+  it 'has a link to edit email addresses' do
+    company.email_addresses.each do |email|
+      expect(page).to have_link('edit', href: edit_email_address_path(email))
+    end
+  end
+  
+  it 'edits an email address' do
+    email = company.email_addresses.first
+    old_email = email.address
+    first(:link, 'edit').click
+    
+    page.fill_in('Address', with: 'newemailaddress@yahoo.com')
+    page.click_button('Update Email address')
+    
+    expect(current_path).to eq(company_path(company))
+    expect(page).to have_content('newemailaddress@yahoo.com')
+    
+    expect(page).to_not have_content(old_email)
+  end
+  
+  it 'deletes an email address' do
+    email = company.email_addresses.first
+    email_address = email.address
+    
+    first(:link, 'delete').click
+    
+    expect(current_path).to eq(company_path(company))
+    expect(page).to_not have_content(email_address)
+  end
+ end
 end
